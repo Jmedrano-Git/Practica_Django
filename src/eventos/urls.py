2f0f3ea40c82da847c22db6e735193bb53a14654
Julio C. Medrano <julio.medrano@tecsup.edu.pe>
2025-09-10 16:54:00 -0500

    xd

--- src/eventos/urls.py
+++ src/eventos/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('nuevo/', views.crear_eventos, name="crear_evento"),
     path('editar/<int:pk>/', views.editar_evento, name="editar_evento"),
     path('eliminar/<int:pk>/', views.eliminar_evento, name="eliminar_evento"),
-    path('asignar-tarea/<int:pk>/', views.asignar_organizador, name="asignar_organizador"),
-    path('tarea/toggle/<int:tarea_id>/', views.toggle_Organizador, name='toggle_organizador'),
+    path('asignar-organizador/<int:pk>/', views.asignar_organizador, name="asignar_organizador"),
+    path('organizador/toggle/<int:organizador_id>/', views.toggle_Organizador, name='toggle_organizador'),
 ]
