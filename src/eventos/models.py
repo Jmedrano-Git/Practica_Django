6d787f223af897e280bff43137d3dbbad0786762
Julio C. Medrano <julio.medrano@tecsup.edu.pe>
2025-09-10 16:14:44 -0500

    Add model Organizadores

--- src/eventos/models.py
+++ src/eventos/models.py
@@ -6,3 +6,14 @@ class EventosDB(models.Model):
     fecha = models.DateTimeField(auto_now_add=True)
     lugar = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.nombre
+
+class OrganizadoresDB(models.Model):
+    evento = models.ForeignKey(EventosDB, on_delete=models.CASCADE, related_name="organizadores")
+    nombre = models.CharField(max_length=100)
+    contacto = models.EmailField(unique=50)
+
+    def __str__(self):
+        return self.nombre
+
