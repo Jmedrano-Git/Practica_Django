e3707a0e092b6bea3a0224529f511225caeb9a56
Julio C. Medrano <julio.medrano@tecsup.edu.pe>
2025-09-10 16:08:49 -0500

    add Model Eventos

--- src/eventos/models.py
+++ src/eventos/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class EventosDB(models.Model):
+    nombre = models.CharField(max_length=100)
+    fecha = models.DateTimeField(auto_now_add=True)
+    lugar = models.CharField(max_length=50)
+
